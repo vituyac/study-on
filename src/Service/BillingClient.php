3bac284da12d190b81ab118f94cff878cfa27cf9
vituyac <skisterev78@mail.ru>
2026-05-04 02:26:53 +0300

    feat(BillingClient): new billing methods

--- src/Service/BillingClient.php
+++ src/Service/BillingClient.php
@@ -32,16 +32,53 @@ class BillingClient
         return $data;
     }
 
+    public function courses(): array
+    {
+        $data = $this->request('GET', '/api/v1/courses', null);
+
+        return $data;
+    }
+
+    public function course(string $code): array
+    {
+        $data = $this->request('GET', '/api/v1/courses/' . $code, null);
+
+        return $data;
+    }
+
+    public function transactions(string $token, array $filters): array
+    {
+        $data = $this->request('GET', '/api/v1/transactions', null, ['Authorization: Bearer ' . $token], $filters);
+
+        return $data;
+    }
+
+    public function pay(string $token, string $code): array
+    {
+        $data = $this->request('POST', '/api/v1/courses/' . $code . '/pay', null, ['Authorization: Bearer ' . $token]);
+
+        return $data;
+    }
+
     public function getCurrentUser(string $token): array
     {
         return $this->request('GET', '/api/v1/users/current', null, ['Authorization: Bearer ' . $token]);
     }
 
-    private function request(string $method, string $uri, ?array $body = null, array $headers = []): array
-    {
+    private function request(
+        string $method,
+        string $uri,
+        ?array $body = null,
+        array $headers = [],
+        array $query = []
+    ): array {
         $curl = curl_init();
 
         $defaultHeaders = ['Content-Type: application/json'];
+
+        if (!empty($query)) {
+            $uri .= '?' . http_build_query($query);
+        }
 
         curl_setopt_array($curl, [
             CURLOPT_URL => rtrim($this->billingBaseUrl, '/') . $uri,
