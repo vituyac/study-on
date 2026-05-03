8538a317efab257700f73388c311c7616b97a2f9
vituyac <skisterev78@mail.ru>
2026-05-04 02:33:41 +0300

    feat(ProfileController): add transaction info

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CourseRepository;
 use App\Service\BillingClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,12 +14,29 @@ final class ProfileController extends AbstractController
 {
     #[Route(name: 'app_profile_show', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
-    public function show(BillingClient $billingClient): Response
+    public function show(BillingClient $billingClient, CourseRepository $courseRepository): Response
     {
-        $user = $billingClient->getCurrentUser($this->getUser()->getApiToken());
+        $user = $this->getUser();
+        $billingUser = $billingClient->getCurrentUser($this->getUser()->getApiToken());
+
+        try {
+            $transactions = $billingClient->transactions($user->getApiToken(), []);
+            $coursesByCode = [];
+            foreach ($courseRepository->findAll() as $course) {
+                $coursesByCode[$course->getCode()] = $course;
+            }
+            $billingAvailable = true;
+        } catch (\Throwable) {
+            $transactions = [];
+            $coursesByCode = [];
+            $billingAvailable = false;
+        }
 
         return $this->render('profile/show.html.twig', [
-            'user' => $user,
+            'user' => $billingUser,
+            'transactions' => $transactions,
+            'coursesByCode' => $coursesByCode,
+            'billingAvailable' => $billingAvailable,
         ]);
     }
 }
